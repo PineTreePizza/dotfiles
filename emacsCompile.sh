1c937164a8326266acf9c6b223ee412418abdf81
Anton Kostochka <77921296+PineTreePizza@users.noreply.github.com>
2023-11-06 16:48:52 +0300

    Create emacsCompile.sh

---
+++ emacsCompile.sh
@@ -0,0 +1,2 @@
+./configure --with-x-toolkit=lucid --without-toolkit-scroll-bars --without-sound --with-native-compilation --with-json --with-mailutils --with-wide-int CFLAGS="-g3 -O2"
+sudo make install
